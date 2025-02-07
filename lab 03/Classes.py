8aae6848ca2b9b67f3f4c8890b68bc921e879d88
ttaermy <tomiristulebaeva37@gmail.com>
2025-02-07 23:55:48 +0500

    initial commit

--- lab 03/Classes.py
+++ lab 03/Classes.py
@@ -1,2 +1,86 @@
-#Define a class which has at least two methods: getString: to get a string from console input printString: to print the string in upper case.
+#1Define a class which has at least two methods: getString: to get a string from console input printString: to print the string in upper case.
 
+class StringManipulator:
+    def __init__(self):
+        self.text = ""
+
+    def getString(self):
+        self.text = input("Enter a string: ")
+
+    def printString(self):
+        print(self.text.upper())
+
+#2
+
+class Shape:
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+
+    def area(self):
+        return self.length * self.length
+
+#3
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+#4
+
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print("Point: (" + str(self.x) + ", " + str(self.y) + ")")
+
+    def move(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+
+    def dist(self, other_point):
+        return math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
+
+#5
+
+class Account:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Deposited " + str(amount) + ". New balance: " + str(self.balance))
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds!")
+        else:
+            self.balance -= amount
+            print("Withdrew " + str(amount) + ". New balance: " + str(self.balance))
+
+#6
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+numbers = [3, 5, 8, 11, 14, 17, 21]
+prime_numbers = list(filter(lambda x: is_prime(x), numbers))
+
+print(prime_numbers)
