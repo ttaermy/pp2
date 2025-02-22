29f31e2032de4ff645124d0f798a640ae20e2782
ttaermy <tomiristulebaeva37@gmail.com>
2025-02-22 15:29:53 +0500

    initial commit

---
+++ lab 04/math.py
@@ -0,0 +1,36 @@
+#1
+import math
+
+degree = 15
+radian = degree * (math.pi/180)
+
+print(radian)
+
+#2
+import math
+
+h = 5
+a = 5
+b = 6
+
+area = 1/2 * (a+b) * h
+print(area)
+
+#3
+import math 
+
+n = int(input())
+s = int(input())
+
+area = (n * s**2) / (4 * math.tan(math.pi / n))
+
+print(area)
+
+#4
+import math
+
+base = 5
+hight = 6
+
+area = base * hight
+print(area)
